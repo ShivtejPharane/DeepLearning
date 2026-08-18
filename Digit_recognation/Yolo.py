a60d40bb4ffb2c98a205984f44392206cf3b04e2
ShivtejPharane <pharaneshiva@gmail.com>
2026-08-18 20:21:41 +0530

    hand written digit

---
+++ Digit_recognation/Yolo.py
@@ -0,0 +1,5 @@
+from ultralytics import YOLO
+import cv2
+
+# Create the Yolo Object
+model = YOLO()
--- Digit_recognation/program2.py
+++ Digit_recognation/program2.py
@@ -4,8 +4,8 @@ import torch.nn as nn
 import cv2
 import matplotlib.pyplot as plt 
 import streamlit as st
-
-
+from PIL import Image
+from torchvision import transforms
 
 # select the deveice
 device = ""
@@ -65,3 +65,60 @@ st.header("Hand Written digit Recognation")
 st.write("Upload a image containing handwritten digit ")
 
 file= st.file_uploader(label="Upload file",type=['png','jpg','jpeg'])
+
+if file is not None:
+    image = Image.open(file).convert("L")
+
+    # convert pillow image to 3d array
+    image = np.array(image)
+
+    st.subheader("Uploaded Image")
+    st.image(image=image,clamp=True)
+
+    #binarize the image
+    return_value,binary_image = cv2.threshold(image,150,255,cv2.THRESH_BINARY_INV)
+
+    points = cv2.findNonZero(binary_image)
+
+    if points is None:
+        st.error("No digit found, Please Upload the Right image")
+    else:
+        # Find the Bounding Image
+        x,y,w,h = cv2.boundingRect(points)
+
+        digit = binary_image[y:y+h,x:x+w]
+
+        size = max(w,h)
+
+        # Make the Square of the Size
+        square = np.zeros((size,size),dtype=np.uint8)
+
+        # Find the offset :
+        x_offset = (size-w)//2
+        y_offset = (size-h)//2
+
+        square[y_offset:y_offset+h,x_offset:x_offset+w] = digit
+
+        # Resize the image for the Models Requirement
+        image_resized = cv2.resize(square,(28,28))
+
+        # Create the Transformer to create the image to craete the tensor
+        transformer = transforms.ToTensor()
+
+        # convert The Image to The Tensor
+        image = transformer(image_resized)
+
+        # unsqueeze the image
+        image = image.unsqueeze(0)
+
+        # Move to the Device 
+        image = image.to(device=device)
+
+        with torch.no_grad():
+            output=model(image)
+            predicted_class = torch.argmax(output,dim=1).item()
+
+            #Show to the User
+            st.success(f"Predicted the Digit = {predicted_class}")
+
+
